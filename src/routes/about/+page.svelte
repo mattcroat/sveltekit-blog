47453db2702f8ceecf7fe1976dcf1e53e582aeb2
mattcroat <mattcroat.dev@gmail.com>
2022-12-07 18:38:07 +0100

    feat: Add About page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,36 @@
+<div class="container">
+	<h1>About</h1>
+
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia similique
+		maiores expedita mollitia ab deleniti autem, eveniet sint reprehenderit
+		earum obcaecati deserunt inventore, quis enim! Dolores saepe beatae quos
+		reiciendis?
+	</p>
+
+	<p>
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, nam.
+		Debitis, similique eum nihil ipsum, itaque eveniet, dignissimos repudiandae
+		aspernatur id quisquam pariatur! Dolores id perferendis quos! Ad, aliquam
+		voluptas.
+	</p>
+</div>
+
+<style lang="postcss">
+	.container {
+		--padding-block: var(--space-4);
+
+		@media (--sm) {
+			--padding-block: var(--space-7);
+		}
+	}
+
+	h1 + * {
+		margin-top: var(--space-2);
+	}
+
+	p {
+		max-width: 60ch;
+		color: var(--clr-text-muted);
+	}
+</style>
