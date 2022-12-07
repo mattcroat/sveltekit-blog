5f8199d2ba3ccae037a8d5004c326c24873d2c68
mattcroat <mattcroat.dev@gmail.com>
2022-12-07 20:53:33 +0100

    feat: Add error pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+<div class="container">
+	<hgroup>
+		<h1>{$page.error?.message}</h1>
+		<h2>The resource you requested is in another castle.</h2>
+	</hgroup>
+
+	<a href="/">Return to the home page</a>.
+</div>
+
+<style>
+	.container {
+		--padding-block: var(--space-7);
+	}
+
+	h1 {
+		text-transform: capitalize;
+	}
+
+	a {
+		color: var(--clr-text);
+		text-decoration: underline;
+		text-underline-offset: 6px;
+	}
+</style>
---
+++ src/routes/admin/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+<div class="container">
+	<hgroup>
+		<h1>{$page.error?.message}</h1>
+		<h2>Try refreshing the page or try again later.</h2>
+	</hgroup>
+</div>
+
+<style>
+	h1 {
+		text-transform: capitalize;
+	}
+</style>
