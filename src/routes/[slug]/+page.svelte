a72535c19e1c8325b7dce5f041619ea24199d018
mattcroat <mattcroat.dev@gmail.com>
2022-12-15 12:44:00 +0100

    style: Post

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -12,3 +12,37 @@
 <main class="prose">
 	{@html html}
 </main>
+
+<style lang="postcss" global>
+	@import 'highlight.js/styles/base16/material.css';
+
+	.prose {
+		@media (--sm) {
+			max-width: 60ch;
+			margin-top: var(--space-7);
+			margin-inline: auto;
+		}
+
+		& pre {
+			border-radius: var(--round-1);
+		}
+
+		& code {
+			font-size: var(--fs-1);
+		}
+
+		& h1 {
+			margin-bottom: var(--space-7);
+			text-align: center;
+		}
+
+		& a {
+			text-decoration: underline;
+			transition: color 0.3s ease;
+
+			&:hover {
+				color: var(--clr-brand);
+			}
+		}
+	}
+</style>
