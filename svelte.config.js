de9ffd57e56719d42f00df4c5dffbdfd0fe9484f
mattcroat <mattcroat.dev@gmail.com>
2022-12-15 13:17:32 +0100

    build: Update Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
 import adapter from '@sveltejs/adapter-auto'
-import preprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [preprocess({ postcss: true })],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	},
 }
 
