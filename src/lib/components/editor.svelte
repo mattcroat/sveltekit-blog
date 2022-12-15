82e05b96354836c24c3d2c54b4f5155f48d38ab8
mattcroat <mattcroat.dev@gmail.com>
2022-12-15 12:42:24 +0100

    style: Move editor styles

--- src/lib/components/editor.svelte
+++ src/lib/components/editor.svelte
@@ -2,9 +2,6 @@
 	import { createEventDispatcher, onMount } from 'svelte'
 	import Editor from '@toast-ui/editor'
 
-	import '@toast-ui/editor/dist/toastui-editor.css'
-	import '@toast-ui/editor/dist/theme/toastui-editor-dark.css'
-
 	export let initialValue: string = ''
 
 	const dispatch = createEventDispatcher()
@@ -43,10 +40,75 @@
 
 <div id="editor" />
 
-<style lang="postcss">
+<style lang="postcss" global>
+	@import '@toast-ui/editor/dist/toastui-editor.css';
+	@import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
+
 	.loading {
 		display: grid;
 		place-content: center;
 		height: 600px;
+	}
+
+	.editor {
+		margin-block: var(--space-2);
+	}
+
+	.editor .ProseMirror {
+		font-size: var(--fs-1);
+		font-family: var(--font-sans);
+	}
+
+	.editor .toastui-editor-defaultUI {
+		font-family: var(--font-sans);
+	}
+
+	.editor .toastui-editor-dark .toastui-editor-md-tab-container,
+	.editor .toastui-editor-dark .toastui-editor-defaultUI-toolbar,
+	.editor .toastui-editor-dark .toastui-editor-md-tab-container .tab-item,
+	.editor .toastui-editor-dark .toastui-editor-mode-switch .tab-item {
+		background: none;
+	}
+
+	.editor .toastui-editor-dark.toastui-editor-defaultUI,
+	.editor .toastui-editor-dark .toastui-editor-md-tab-container .tab-item,
+	.editor .toastui-editor-dark .toastui-editor-toolbar-icons,
+	.editor .toastui-editor-dark .toastui-editor-mode-switch .tab-item {
+		border: 1px solid var(--form-element-border-color);
+	}
+
+	.editor .toastui-editor-dark .toastui-editor-defaultUI-toolbar,
+	.editor .toastui-editor-dark .toastui-editor-md-tab-container {
+		border-bottom-color: var(--form-element-border-color);
+	}
+
+	.editor .toastui-editor-dark .toastui-editor-mode-switch {
+		border-top-color: var(--form-element-border-color);
+	}
+
+	.editor
+		.toastui-editor-dark
+		.toastui-editor-md-tab-container
+		.tab-item.active {
+		background: none;
+	}
+
+	.editor .toastui-editor-dark .toastui-editor-md-container,
+	.editor .toastui-editor-dark .toastui-editor-ww-container,
+	.editor .toastui-editor-dark .toastui-editor-mode-switch {
+		background: none;
+	}
+
+	.editor .toastui-editor-dark .toastui-editor-dropdown-toolbar {
+		border-color: var(--form-element-border-color);
+		background: none;
+	}
+
+	.editor .toastui-editor-dark .toastui-editor-mode-switch .tab-item.active {
+		background: none;
+	}
+
+	.toastui-editor-dark .toastui-editor-md-code-block-line-background {
+		background: none;
 	}
 </style>
